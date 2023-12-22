7e67eda7b4e9aa32179e9e03af8f65f34924fa93
Maxim-g1 <122662234+Maxim-g1@users.noreply.github.com>
2023-12-22 14:59:34 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,12 +1,45 @@
-let button = document.getElementById("convert");
-let input = document.getElementById("one");
-let input2=document.getElementById("two")
-let output = document.getElementById("three");
+let button = document.getElementById("equals");
+let input = document.getElementById("num1");
+let input2=document.getElementById("num2")
+let output = document.getElementById("result");
 let select=document.getElementById('select')
-function print() {
-    let one = Number(input.value);   
-let two = Number(input2.value);  
-  let znak=(select.value) 
-    output.value = one+znak +two;
+
+function plus(number1, number2){
+  return Number(number1.value) + Number(number2.value);
 }
-button.addEventListener("click", print); 
+
+function minus(number1, number2){
+  return number1.value - number2.value;
+}
+
+function umnoj(number1, number2){
+  return number1.value * number2.value;
+}
+
+function deli(number1, number2){
+  return number1.value / number2.value;
+}
+
+function ostatok(number1, number2){
+  return number1.value % number2.value;
+}
+
+equals.addEventListener('click', function(){
+if(select.value == '+'){
+  result.value = plus(num1, num2)
+}
+if(select.value == '-') {
+  result.value = minus(num1, num2)
+}
+if(select.value == '*') {
+  result.value = umnoj(num1, num2)
+}
+if(select.value == '/') {
+  result.value = deli(num1, num2)
+}
+if(select.value == '%') {
+  result.value = ostatok(num1, num2)
+}
+})
+
+
